aca14112a3481b232481afb1b9a1b188a14863bf
Jiri Hysek <jiri.hysek@gmail.com>
2013-06-08 18:03:22 +0200

    Config generator injects module into application_controller

--- lib/generators/cdv_session/config_generator.rb
+++ lib/generators/cdv_session/config_generator.rb
@@ -3,7 +3,15 @@ module CdvSession
     def create_initializer_file
       create_file "config/initializers/cdv_session.rb",
         "CdvSession.application_code = 'app'\t # nastavte kod teto aplikace\n" +
-        "CdvSession.accounts_service_url = 'http://localhost:3001'\t # nastavte URL, na ktere komunikuje AccountsService"
+        "CdvSession.accounts_service_url = 'http://localhost:3001'\t " +
+        "# nastavte URL, na ktere komunikuje AccountsService"
+    end
+
+    def turn_on_authentication
+      inject_into_file 'app/controllers/application_controller.rb',
+        after: "class ApplicationController < ActionController::Base" do
+         "\n  include CdvSession::Authenticate\n"
+      end
     end
 
     def mount_engine
